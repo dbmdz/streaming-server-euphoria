ec797b92754962d6005d2ef43c64d2b1e75632fc
Matthias Lindinger <matthias.lindinger@bsb-muenchen.de>
2020-07-10 14:51:45 +0200

    Refactor webjar version to configuration properties

--- src/main/java/de/digitalcollections/streaming/euphoria/Application.java
+++ src/main/java/de/digitalcollections/streaming/euphoria/Application.java
@@ -2,7 +2,9 @@ package de.digitalcollections.streaming.euphoria;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+@ConfigurationPropertiesScan
 @SpringBootApplication
 public class Application {
 
