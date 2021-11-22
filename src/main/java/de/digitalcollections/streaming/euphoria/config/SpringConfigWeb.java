b9c288e9144c5770adc55e0df272611a5b6d52fe
Matthias Lindinger <matthias.lindinger@bsb-muenchen.de>
2021-11-22 09:28:47 +0100

    Fix import

--- src/main/java/de/digitalcollections/streaming/euphoria/config/SpringConfigWeb.java
+++ src/main/java/de/digitalcollections/streaming/euphoria/config/SpringConfigWeb.java
@@ -2,7 +2,7 @@ package de.digitalcollections.streaming.euphoria.config;
 
 import de.digitalcollections.commons.springmvc.interceptors.CurrentUrlAsModelAttributeHandlerInterceptor;
 import java.util.Locale;
-import nz.net.ultraq.thymeleaf.LayoutDialect;
+import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
